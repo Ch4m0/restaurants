c3e8597b5f7df9401b70a0e3163e4de86fb2f4da
Abner <abachadi@gmail.com>
2016-05-26 23:36:56 -0500

    seeds

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => '1',
+            'name'=>'The3ballsoft',
+ 			'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\User::class, 30)->create();
+    }
+}
