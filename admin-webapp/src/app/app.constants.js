42e0f55e9c79b45826da80e34515506cf732fbb9
Abner <abachadi@gmail.com>
2016-05-26 22:41:57 -0500

    url chang

--- admin-webapp/src/app/app.constants.js
+++ admin-webapp/src/app/app.constants.js
@@ -1,7 +1,7 @@
 
   angular.module("app").constant("CONFIG", {
-    ROOT_URL: "https://immense-retreat-62506.herokuapp.com",
-    API_URL: "https://immense-retreat-62506.herokuapp.com"
+    ROOT_URL: "https://immense-retreat-62506.herokuapp.com/",
+    API_URL: "https://immense-retreat-62506.herokuapp.com/api/"
   }).constant("AUTH_EVENTS", {
     notAuthenticated: "auth-not-authenticated", 
     notAuthorized: "auth-not-authorized",
--- public/webapp/scripts/app.constants.js
+++ public/webapp/scripts/app.constants.js
@@ -1,5 +1,5 @@
  angular.module("Client").constant("CONFIG", {
     ROOT_URL: "https://immense-retreat-62506.herokuapp.com",
-    API_URL: "https://immense-retreat-62506.herokuapp.com",
+    API_URL: "https://immense-retreat-62506.herokuapp.com/api/",
     PUBLIC_URL: "https://immense-retreat-62506.herokuapp.com"
   })
