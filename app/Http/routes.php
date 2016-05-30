79b925a27292bf4779c1d335d502e825efb76a42
Abner <abachadi@gmail.com>
2016-05-30 10:08:18 -0500

    todo bien

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Credentials: true');
 //header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, PATCH, DELETE');
 //header("Access-Control-Allow-Headers: Authorization, X-Requested-With,  Content-Type, Accept");
