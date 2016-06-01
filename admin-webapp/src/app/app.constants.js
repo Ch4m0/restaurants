6c59ffa834335ab7b19e1050f06ed8a6ca0da01b
Abner <abachadi@gmail.com>
2016-06-01 17:15:44 -0500

    urls

--- admin-webapp/src/app/app.constants.js
+++ admin-webapp/src/app/app.constants.js
@@ -4,8 +4,8 @@
     // ROOT_URL: "localhost:8000",
     // API_URL: "localhost:8000/api/"
 
-    ROOT_URL: "https://immense-retreat-62506.herokuapp.com/",
-    API_URL: "https://immense-retreat-62506.herokuapp.com/api/"
+    ROOT_URL: "http://srestaurants.herokuapp.com/",
+    API_URL: "https://srestaurants.herokuapp.com/api/"
   }).constant("AUTH_EVENTS", {
     notAuthenticated: "auth-not-authenticated", 
     notAuthorized: "auth-not-authorized",
--- public/webapp/scripts/app.constants.js
+++ public/webapp/scripts/app.constants.js
@@ -1,7 +1,7 @@
  angular.module("Client").constant("CONFIG", {
-    ROOT_URL: "https://immense-retreat-62506.herokuapp.com",
-    API_URL: "https://immense-retreat-62506.herokuapp.com/api/",
-    PUBLIC_URL: "https://immense-retreat-62506.herokuapp.com"
+    ROOT_URL: "http://srestaurants.herokuapp.com",
+    API_URL: "http://srestaurants.herokuapp.com/api/",
+    PUBLIC_URL: "http://srestaurants.herokuapp.com"
     // ROOT_URL: "localhost:8000",
     // API_URL: "localhost:8000/api/"
   })
